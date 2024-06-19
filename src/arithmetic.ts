0ed1c1b76512db7989406c8f527bb2a3ca0edd57
Gabriele Labanca <gabrielelabanca@gmail.com>
2024-06-19 12:25:47 +0200

    ia

--- src/arithmetic.ts
+++ src/arithmetic.ts
@@ -2,3 +2,8 @@ function sum(a, b) {
   return a + b;
 }
 module.exports.sum = sum; 
+
+function i2ia(s){
+  return s.replaceAll('i', 'ia');
+}
+module.exports.i2ia = i2ia; 
--- test/arithmetic.test.ts
+++ test/arithmetic.test.ts
@@ -1,5 +1,12 @@
-const sum = require('../src/arithmetic').sum;
+// const sum = require('../src/arithmetic').sum;
+// const i2ia = require('../src/arithmetic').i2ia;
+const ari = require('../src/arithmetic');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(ari.sum(1, 2)).toBe(3);
 }); 
+
+test('i becomes ia', () => {
+  expect(ari.i2ia('pino')).toBe('piano');
+  expect(ari.i2ia('ghii')).toBe('ghiaia');
+})
